dc6c1396716438d7f29ff0d7d682fc15cf32a996
Safiya Cain <say.cain3@gmail.com>
2020-10-08 12:24:19 -0400

    Complete animation for disappearing mole head

--- mole.js
+++ mole.js
@@ -1,1 +1,7 @@
 // Write your JS here.
+setInterval(() => {
+    const moleHeads = document.querySelectorAll(".wgs__mole-head");
+    for(let moleHead of moleHeads){
+        moleHead.classList.toggle("wgs__mole-head--hidden");
+    }
+}, 1000);
