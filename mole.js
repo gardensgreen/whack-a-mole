ce709671568fb7d0215c9812ec115bc198535b2a
Daniel TIllero <danieltillero67@gmail.com>
2020-10-08 12:32:52 -0400

    create playing field

--- mole.js
+++ mole.js
@@ -1,7 +1,9 @@
 // Write your JS here.
-setInterval(() => {
-    const moleHeads = document.querySelectorAll(".wgs__mole-head");
-    for(let moleHead of moleHeads){
-        moleHead.classList.toggle("wgs__mole-head--hidden");
-    }
-}, 1000);
+
+// **Test Animation**
+// setInterval(() => {
+//     const moleHeads = document.querySelectorAll(".wgs__mole-head");
+//     for(let moleHead of moleHeads){
+//         moleHead.classList.toggle("wgs__mole-head--hidden");
+//     }
+// }, 1000);
